c46be693d94f7908df0c6ee5d5f6a0969852d32a
Zoey Jacques <zoey.jacques630@gmail.com>
2018-12-15 13:03:18 -0500

    styleguide fixes

--- app/javascript/tests/pages/SignInPage/mapStateToProps.test.js
+++ app/javascript/tests/pages/SignInPage/mapStateToProps.test.js
@@ -1,15 +1,15 @@
-import mapStateToProps from '../../../pages/SignInPage/mapStateToProps';
+import mapStateToProps from "../../../pages/SignInPage/mapStateToProps";
 
-describe('mapStateToProps', () => {
+describe("mapStateToProps", () => {
   const state = {
     user: {
-      loginErrorMessage: 'Hey',
+      loginErrorMessage: "Hey",
     },
   };
 
   const props = mapStateToProps(state);
 
-  it('contains the login error message', () => {
-    expect(props.loginErrorMessage).toEqual('Hey');
+  it("contains the login error message", () => {
+    expect(props.loginErrorMessage).toEqual("Hey");
   });
 });
