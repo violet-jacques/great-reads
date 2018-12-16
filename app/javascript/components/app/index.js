af3612a2d13f5f5ef856104691b2c49f1cc4d4d1
Zoey Jacques <zoey.jacques630@gmail.com>
2018-12-16 17:18:08 -0500

    fix routing issue

--- app/javascript/components/app/index.js
+++ app/javascript/components/app/index.js
@@ -1,10 +1,13 @@
+import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 
 import App from "./component";
 import mapStateToProps from "./mapStateToProps";
 import mapDispatchToProps from "./mapDispatchToProps";
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(App)
+);
