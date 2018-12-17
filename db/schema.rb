b19b7a205cda50eeea160109694fa1ba5457f99d
michael-jacques <michael.jacques630@gmail.com>
2018-12-16 20:08:44 -0500

    backpopulate timestamps

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181217002910) do
+ActiveRecord::Schema.define(version: 20181217010649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 20181217002910) do
   create_table "books", force: :cascade do |t|
     t.string "title", null: false
     t.text "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "genre_categorizations", force: :cascade do |t|
@@ -58,6 +60,8 @@ ActiveRecord::Schema.define(version: 20181217002910) do
   create_table "genres", force: :cascade do |t|
     t.string "name"
     t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["name"], name: "index_genres_on_name", unique: true
   end
 
@@ -91,6 +95,8 @@ ActiveRecord::Schema.define(version: 20181217002910) do
     t.integer "format", null: false
     t.integer "pages", null: false
     t.bigint "book_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["book_id"], name: "index_variants_on_book_id"
     t.index ["format"], name: "index_variants_on_format"
     t.index ["isbn"], name: "index_variants_on_isbn", unique: true
