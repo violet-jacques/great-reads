b04fb23ffe13c80d091daebab387a8108479a1fc
Zoey Jacques <zoey.jacques630@gmail.com>
2018-12-16 20:03:11 -0500

    create influences

---
+++ spec/models/influence_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Influence, type: :model do
+  describe "validations" do
+    it { should validate_presence_of(:influencer_id) }
+    it { should validate_presence_of(:influencee_id) }
+  end
+
+  describe "associations" do
+    it { should belong_to(:influencer).class_name("Author") }
+    it { should belong_to(:influencee).class_name("Author") }
+  end
+end
