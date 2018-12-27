7640fdd561907d7a6b0705c82f47572d98c23bb7
michael-jacques <michael.jacques630@gmail.com>
2018-12-26 20:44:30 -0500

    back end for searching books
    initial search controller that searches for books based on a query string. the search looks at book title, author first and last name, and genre. this uses the pg search gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    sessions: "users/sessions",
-    registrations: "users/registrations",
+    sessions: "api/users/sessions",
+    registrations: "api/users/registrations",
   }
+
+  namespace :api do
+    resources :searches, only: [:index]
+  end
+
   get "/" => "home#home"
-  get "/*path", to: "home#home"
 
   devise_scope :user do
     post "api/users/sign_in", to: "api/users/sessions#create"
     post "api/users/sign_up", to: "api/users/registrations#create"
   end
+
+  get "/*path", to: "home#home"
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -15,4 +15,70 @@ RSpec.describe Book, type: :model do
     it { should have_many(:user_books) }
     it { should have_many(:users).through(:user_books) }
   end
+
+  describe "scopes" do
+    describe ".book_search" do
+      let(:search_term) { nil }
+      let(:matching_book) do
+        create(
+          :book,
+          title: "Sup Dog",
+          variants: create_list(:variant, 2),
+        )
+      end
+      let(:matching_genre) { create(:genre, name: "Fiction") }
+      let!(:matching_genre_categorization) do
+        create(:genre_categorization, book: matching_book, genre: matching_genre)
+      end
+      let(:matching_author) { create(:author) }
+      let!(:matching_author_book) do
+        create(:author_book, book: matching_book, author: matching_author)
+      end
+      let(:non_matching_book) { create(:book, title: "Cat") }
+      let(:non_matching_genre) { create(:genre, name: "Horror") }
+      let!(:non_matching_genre_categorization) do
+        create(:genre_categorization, book: matching_book, genre: non_matching_genre)
+      end
+      let(:non_matching_author) { create(:author, first_name: "John", last_name: "Smith") }
+      let!(:non_matching_author_book) do
+        create(:author_book, book: non_matching_book, author: non_matching_author)
+      end
+
+      context "searching by title" do
+        let(:search_term) { "Dog" }
+
+        it "returns all books with title dog" do
+          expect(Book.book_search(search_term))
+            .to contain_exactly(matching_book)
+        end
+      end
+
+      context "searching by author first name" do
+        let(:search_term) { "Jane" }
+
+        it "returns all books with author named Jane" do
+          expect(Book.book_search(search_term))
+            .to contain_exactly(matching_book)
+        end
+      end
+
+      context "searching by author last name" do
+        let(:search_term) { "Author" }
+
+        it "returns all books with author named Jane" do
+          expect(Book.book_search(search_term))
+            .to contain_exactly(matching_book)
+        end
+      end
+
+      context "searching by genre" do
+        let(:search_term) { "Fiction" }
+
+        it "returns all books with author named Jane" do
+          expect(Book.book_search(search_term))
+            .to contain_exactly(matching_book)
+        end
+      end
+    end
+  end
 end
