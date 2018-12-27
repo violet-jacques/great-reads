5d206092cd7e7a38cad8e9ff8c4223f5d0c2cd81
Violet Jacques <violet.jacques630@gmail.com>
2018-12-26 20:44:30 -0500

    back end for searching books
    initial search controller that searches for books based on a query string. the search looks at book title, author first and last name, and genre. this uses the pg search gem

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
+  include PgSearch
+
   validates :title, presence: true
   validates :description, presence: true
 
@@ -9,4 +11,12 @@ class Book < ApplicationRecord
   has_many :authors, through: :author_books
   has_many :user_books
   has_many :users, through: :user_books
+
+  pg_search_scope :book_search,
+    against: [:title],
+    associated_against: {
+      authors: [:first_name, :last_name],
+      genres: [:name]
+    },
+    using: { tsearch: { prefix: true } }
 end
