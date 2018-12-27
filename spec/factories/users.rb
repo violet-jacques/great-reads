a98bd0d050351153db636dcbdc47c80f251846b8
michael-jacques <michael.jacques630@gmail.com>
2018-12-26 21:21:40 -0500

    add rating count and average rating to search results

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     first_name { "User" }
     last_name { "Name" }
-    email { "test@example.com" }
+    sequence(:email) { |n| "test#{n}@example.com" }
     password { "password" }
   end
 end
