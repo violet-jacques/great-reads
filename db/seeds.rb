2ec55da8e04235c822c6db665835cbccaa607c31
Zoey Jacques <zoey.jacques630@gmail.com>
2018-12-26 19:23:44 -0500

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,88 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "factory_bot_rails"
+require "ffaker"
+
+if Rails.env.development?
+  GenreCategorization.destroy_all
+  AuthorBook.destroy_all
+  Influence.destroy_all
+  Variant.destroy_all
+  Book.destroy_all
+  Genre.destroy_all
+  Author.destroy_all
+
+  [
+    "Absurdist", "Tragedy", "Science Fiction", "Fantasy",
+    "Adventure", "Mystery", "Satire", "Horror", "Fiction",
+  ].each do |name|
+    FactoryBot.create(
+      :genre,
+      name: name,
+      description: FFaker::Lorem.paragraph,
+    )
+  end
+
+  authors = FactoryBot.create_list(
+    :author,
+    100,
+    first_name: FFaker::Name.first_name,
+    last_name: FFaker::Name.last_name,
+    born_at: FFaker::Time.between(100.years.ago, 18.years.ago),
+    birth_place: FFaker::Address.city,
+    description: FFaker::Lorem.paragraph,
+  )
+
+  books = FactoryBot.create_list(
+    :book,
+    600,
+    title: FFaker::Book.title,
+    description: FFaker::Book.description,
+    variants: FactoryBot.create_list(
+      :variant,
+      rand(1..4),
+      format: [:paperback, :hardcover].sample,
+      pages: rand(150...600),
+      published_at: FFaker::Time.datetime,
+      published_by: FFaker::Company.name,
+      language: FFaker::Locale.language,
+    ),
+  )
+
+  authors.each do |author|
+    FactoryBot.create(
+      :author_book,
+      book: books.sample,
+      author: author,
+    )
+  end
+
+  books.each do |book|
+    FactoryBot.create(
+      :author_book,
+      book: book,
+      author: authors.sample,
+    )
+  end
+
+  books.each do |book|
+    FactoryBot.create(
+      :genre_categorization,
+      book: book,
+      genre: Genre.all.sample,
+    )
+  end
+
+  books.sample(Book.count - 20).each do |book|
+    FactoryBot.create(
+      :genre_categorization,
+      book: book,
+      genre: Genre.all.sample,
+    )
+  end
+
+  30.times do
+    Influence.create(
+      influencee_id: authors.sample,
+      influencer_id: authors.sample,
+    )
+  end
+end
