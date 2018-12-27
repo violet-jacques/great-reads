911f1402ca47993a28af003edf8ea1fb669000be
michael-jacques <michael.jacques630@gmail.com>
2018-12-26 20:44:30 -0500

    back end for searching books
    initial search controller that searches for books based on a query string. the search looks at book title, author first and last name, and genre. this uses the pg search gem

---
+++ app/controllers/api/searches_controller.rb
@@ -0,0 +1,35 @@
+module Api
+  class SearchesController < ApplicationController
+    def index
+      render json: { success: true, books: books }
+    end
+
+    private
+
+    def books
+      Book
+        .includes(:variants, :authors)
+        .book_search(search_params[:query])
+        .map { |book| to_h(book) }
+    end
+
+    def search_params
+      params.require(:search).permit(
+        :query,
+      )
+    end
+
+    def to_h(book)
+      {
+        title: book.title,
+        authors: book.authors.map do |author|
+          {
+            first_name: author.first_name,
+            last_name: author.last_name,
+          }
+        end,
+        variant_count: book.variants.count,
+      }
+    end
+  end
+end
---
+++ spec/requests/api/search_request_spec.rb
@@ -0,0 +1,132 @@
+require "rails_helper"
+
+module Api
+  RSpec.describe SearchesController, :type => :request do
+    describe "#index" do
+      let(:query) { nil }
+      let(:params) do
+        {
+          search: {
+            query: query,
+          }
+        }
+      end
+      let(:matching_book) do
+        create(
+          :book,
+          title: "Dog",
+          variants: create_list(:variant, 2),
+        )
+      end
+      let(:matching_genre) { create(:genre, name: "Fiction") }
+      let!(:matching_genre_categorization) do
+        create(:genre_categorization, book: matching_book, genre: matching_genre)
+      end
+      let(:matching_author) { create(:author) }
+      let!(:matching_author_book) do
+        create(:author_book, book: matching_book, author: matching_author)
+      end
+      let(:non_matching_book) { create(:book, title: "Cat") }
+      let(:non_matching_genre) { create(:genre, name: "Horror") }
+      let!(:non_matching_genre_categorization) do
+        create(:genre_categorization, book: matching_book, genre: non_matching_genre)
+      end
+      let(:non_matching_author) { create(:author, first_name: "John", last_name: "Smith") }
+      let!(:non_matching_author_book) do
+        create(:author_book, book: non_matching_book, author: non_matching_author)
+      end
+
+      context "searching by book title" do
+        let(:query) { "Dog" }
+
+        it "returns books matching that title" do
+          get api_searches_path, :params => params
+
+          response = JSON.parse(body)
+
+          expect(response).to eq(
+            "success" => true,
+            "books" => [
+              {
+                "title" => "Dog",
+                "authors" => [
+                  { "first_name" => "Jane", "last_name" => "Author" },
+                ],
+                "variant_count" => 2,
+              }
+            ],
+          )
+        end
+      end
+
+      context "searching by author first_name" do
+        let(:query) { "Jane" }
+
+        it "returns books matching that author first name" do
+          get api_searches_path, :params => params
+
+          response = JSON.parse(body)
+
+          expect(response).to eq(
+            "success" => true,
+            "books" => [
+              {
+                "title" => "Dog",
+                "authors" => [
+                  { "first_name" => "Jane", "last_name" => "Author" },
+                ],
+                "variant_count" => 2,
+              }
+            ],
+          )
+        end
+      end
+
+      context "searching by author last_name" do
+        let(:query) { "Author" }
+
+        it "returns books matching that author first name" do
+          get api_searches_path, :params => params
+
+          response = JSON.parse(body)
+
+          expect(response).to eq(
+            "success" => true,
+            "books" => [
+              {
+                "title" => "Dog",
+                "authors" => [
+                  { "first_name" => "Jane", "last_name" => "Author" },
+                ],
+                "variant_count" => 2,
+              }
+            ],
+          )
+        end
+      end
+
+      context "searching by author last_name" do
+        let(:query) { "Fiction" }
+
+        it "returns books matching that author first name" do
+          get api_searches_path, :params => params
+
+          response = JSON.parse(body)
+
+          expect(response).to eq(
+            "success" => true,
+            "books" => [
+              {
+                "title" => "Dog",
+                "authors" => [
+                  { "first_name" => "Jane", "last_name" => "Author" },
+                ],
+                "variant_count" => 2,
+              }
+            ],
+          )
+        end
+      end
+    end
+  end
+end
