8088eeb672dc1f6c5b596a6ba97cae8f345370a1
Violet Jacques <violet.jacques630@gmail.com>
2019-06-12 17:53:31 -0400

    update database schema date

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190104021626) do
+ActiveRecord::Schema.define(version: 2019_01_04_021626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
