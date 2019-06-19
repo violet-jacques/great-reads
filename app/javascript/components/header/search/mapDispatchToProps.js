8dc90772f714f1c09fa587a5d063a9d096c356a1
mike jacques <michael.jacques630@gmail.com>
2019-06-19 18:43:38 -0400

    adds map dispatch to props for search component
    This change exposes the search action for use in the search components form. It also handles redirecting to "/search"

---
+++ app/javascript/components/header/search/mapDispatchToProps.js
@@ -0,0 +1,12 @@
+import searchActions from "../../../actions/searchActions";
+
+export default (dispatch, ownProps) => {
+  const { search } = searchActions;
+
+  return {
+    onSubmit: query => {
+      ownProps.history.push("/search");
+      return dispatch(search(query));
+    },
+  };
+};
