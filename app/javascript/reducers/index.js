cd458d049973b046e1988adfa1c97f21f38bf9f9
Violet Jacques <violet.jacques630@gmail.com>
2019-06-19 18:39:44 -0400

    add the search reducer
    this reducer just swaps the books value to the current payload

--- app/javascript/reducers/index.js
+++ app/javascript/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux-immutable";
 import { reducer as formReducer } from "redux-form";
 
+import search from "./search";
 import header from "./header";
 import modal from "./modal";
 import user from "./user";
@@ -9,5 +10,6 @@ export default combineReducers({
   header,
   form: formReducer,
   modal,
+  search,
   user,
 });
