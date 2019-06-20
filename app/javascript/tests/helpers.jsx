e16e664aaa930d54ac2b775ecbe51125a8d278a3
Violet Jacques <violet.jacques630@gmail.com>
2019-06-20 15:51:36 -0400

    Update test helpers
    This changes replaces the default state in the test helper functions with an immutable map as opposed to a javascript object

--- app/javascript/tests/helpers.jsx
+++ app/javascript/tests/helpers.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Map } from "immutable";
 import configureStore from "redux-mock-store";
 import { MemoryRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { mount } from "enzyme";
 
-const mountWithStore = (component, state = {}) => {
+const mountWithStore = (component, state = Map()) => {
   const mockStore = configureStore();
   const store = mockStore(state);
 
@@ -15,7 +16,7 @@ const mountWithStore = (component, state = {}) => {
   );
 };
 
-const mountWithRouter = (component, state = {}) => {
+const mountWithRouter = (component, state = Map()) => {
   const wrappedComponent = (
     <Router>
       {component}
