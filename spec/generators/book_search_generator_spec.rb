53ac5c7c5814bcfdac125c114945d19d050dc8ba
mike jacques <michael.jacques630@gmail.com>
2019-06-20 15:48:14 -0400

    Create BookSearchGenerator
    This change adds the BookSearchGenerator for passing search params and a scope. t searches the books table based on the provided scope

---
+++ spec/generators/book_search_generator_spec.rb
@@ -0,0 +1,75 @@
+require "rails_helper"
+
+RSpec.describe BookSearchGenerator do
+  describe ".generate" do
+    let(:scope) { nil }
+    let(:query) { "hey" }
+    subject(:book_search) do
+      described_class.generate(query: query, scope: scope)
+    end
+
+    before do
+      allow(Book).to receive(:general_search)
+      allow(Book).to receive(:title_search)
+      allow(Book).to receive(:author_search)
+      allow(Book).to receive(:genre_search)
+
+      book_search
+    end
+
+    context "scope is 'all'" do
+      let(:scope) { "all" }
+
+      it "performs a general search" do
+        expect(Book).to have_received(:general_search).with(query)
+        expect(Book).not_to have_received(:title_search).with(query)
+        expect(Book).not_to have_received(:author_search).with(query)
+        expect(Book).not_to have_received(:genre_search).with(query)
+      end
+    end
+
+    context "scope is 'title'" do
+      let(:scope) { "title" }
+
+      it "performs a title search" do
+        expect(Book).to have_received(:title_search).with(query)
+        expect(Book).not_to have_received(:general_search).with(query)
+        expect(Book).not_to have_received(:author_search).with(query)
+        expect(Book).not_to have_received(:genre_search).with(query)
+      end
+    end
+
+    context "scope is 'genre'" do
+      let(:scope) { "genre" }
+
+      it "performs a genre search" do
+        expect(Book).to have_received(:genre_search).with(query)
+        expect(Book).not_to have_received(:title_search).with(query)
+        expect(Book).not_to have_received(:author_search).with(query)
+        expect(Book).not_to have_received(:general_search).with(query)
+      end
+    end
+
+    context "scope is 'author'" do
+      let(:scope) { "author" }
+
+      it "performs an author search" do
+        expect(Book).to have_received(:author_search).with(query)
+        expect(Book).not_to have_received(:title_search).with(query)
+        expect(Book).not_to have_received(:general_search).with(query)
+        expect(Book).not_to have_received(:genre_search).with(query)
+      end
+    end
+
+    context "no scope provided" do
+      let(:scope) { nil }
+
+      it "performs a general search" do
+        expect(Book).to have_received(:general_search).with(query)
+        expect(Book).not_to have_received(:title_search).with(query)
+        expect(Book).not_to have_received(:author_search).with(query)
+        expect(Book).not_to have_received(:genre_search).with(query)
+      end
+    end
+  end
+end
