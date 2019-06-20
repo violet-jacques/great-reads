76d8f7cdd086a227b2c7c4453ac08f5ce8dc176b
mike jacques <michael.jacques630@gmail.com>
2019-06-19 20:07:51 -0400

    exposes search page to router
    This change exposes the search page to the router by adding it to the route config

--- app/javascript/routes/routeConfig/index.js
+++ app/javascript/routes/routeConfig/index.js
@@ -1,5 +1,6 @@
 import { List } from "immutable";
 
 import home from "./home";
+import search from "./search";
 
-export default List.of(home);
+export default List.of(home, search);
---
+++ app/javascript/routes/routeConfig/search.js
@@ -0,0 +1,9 @@
+import { Map } from "immutable";
+
+import Search from "../../pages/search";
+
+export default Map({
+  path: "/search",
+  exact: true,
+  component: Search,
+});
