4e03f67f9724d0922df0c9493265b0b75ef32ce8
Zoey Jacques <zoey.jacques630@gmail.com>
2019-06-20 15:54:39 -0400

    Update search actions
    This change updates the search action "search" to handle scope

--- app/javascript/actions/searchActions.js
+++ app/javascript/actions/searchActions.js
@@ -1,20 +1,26 @@
+import { Map } from "immutable";
+
 import searchApi from "../api/searchApi";
 import genericAction from "./genericAction";
 import { SEARCH_SUCCESS } from "../actionTypes";
 
-const handleResponse = response => {
+const handleResponse = (form, response) => {
   return genericAction(
     SEARCH_SUCCESS,
-    response.get("books"),
+    Map({
+      books: response.get("books"),
+      query: form.query,
+      scope: form.scope,
+    }),
   );
 };
 
 export default {
-  search: query => (
+  search: form => (
     dispatch => (
-      searchApi.search(query)
+      searchApi.search(form)
         .then(response => {
-          dispatch(handleResponse(response));
+          dispatch(handleResponse(form, response));
         })
     )
   ),
