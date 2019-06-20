add60bb22019f39c4c0ccbc4769e8a10062584bf
mike jacques <michael.jacques630@gmail.com>
2019-06-20 16:00:29 -0400

    preparing state for search bar component
    This change adds the state to inject into the search bar component

---
+++ app/javascript/tests/components/search/searchBar/mapDispatchToProps.test.js
@@ -0,0 +1,41 @@
+import configureMockStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import { List, Map } from "immutable";
+
+import mapDispatchToProps from "../../../../components/search/searchBar/mapDispatchToProps";
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+describe("mapDispatchToProps", () => {
+  const store = mockStore();
+
+  describe("onSubmit", () => {
+    const props = mapDispatchToProps(store.dispatch);
+    const { onSubmit } = props;
+    const form = Map({ query: "yo dog", scope: "all" });
+
+    beforeEach(() => {
+      global.fetch.resetMocks();
+      global.fetch.mockResponse(JSON.stringify({
+        success: true,
+        books: [{ id: 1, name: "dog" }],
+      }));
+    });
+
+    it("dispatches the correct actions", () => {
+      const expectedAction = {
+        payload: Map({
+          books: List([Map({ id: 1, name: "dog" })]),
+          query: "yo dog",
+          scope: "all",
+        }),
+        type: "SEARCH_SUCCESS",
+      };
+      onSubmit(form)
+        .then(() => {
+          expect(store.getActions()).toContainEqual(expectedAction);
+        });
+    });
+  });
+});
