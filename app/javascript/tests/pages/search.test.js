e703c42b84b640a50838a5f9c34a4aec56bf74d2
Zoey Jacques <zoey.jacques630@gmail.com>
2019-06-19 20:07:22 -0400

    add search page
    This change adds the search page and brings all the search components together

---
+++ app/javascript/tests/pages/search.test.js
@@ -0,0 +1,36 @@
+import React from "react";
+import testHelpers from "../helpers";
+import Search from "../../pages/search";
+
+import AddBook from "../../components/search/addBook";
+import Books from "../../components/search/books";
+import RelatedShelves from "../../components/search/relatedShelves";
+import SearchBar from "../../components/search/searchBar";
+
+describe("Search", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = testHelpers.mount(<Search />);
+  });
+
+  it("renders", () => {
+    expect(wrapper.find(Search)).toHaveLength(1);
+  });
+
+  it("renders a SearchBar", () => {
+    expect(wrapper.find(SearchBar)).toHaveLength(1);
+  });
+
+  it("renders a Books", () => {
+    expect(wrapper.find(Books)).toHaveLength(1);
+  });
+
+  it("renders a AddBook", () => {
+    expect(wrapper.find(AddBook)).toHaveLength(1);
+  });
+
+  it("renders a RelatedShelves", () => {
+    expect(wrapper.find(RelatedShelves)).toHaveLength(1);
+  });
+});
