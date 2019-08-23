2c71d16f1507fb78a1c918a37b2a8e67b12a5071
Violet Jacques <violet.jacques630@gmail.com>
2019-08-22 21:04:09 -0400

    UserLoggedIn state
    This change introduces the concept of shared state for components. These functions are (or are reducible to given args) mapStateToProps functions that can be shared

--- app/javascript/components/header/account/index.js
+++ app/javascript/components/header/account/index.js
@@ -2,7 +2,7 @@ import { connect } from "react-redux";
 
 import Account from "./component";
 import mapDispatchToProps from "./mapDispatchToProps";
-import mapStateToProps from "./mapStateToProps";
+import mapStateToProps from "../../../states/userLoggedIn";
 
 
 export default connect(
